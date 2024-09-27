cd2b67ddd2cb49f27e39b2f6276d91b2d44c100e
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-09-27 15:48:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    TOKEN = os.environ.get("TOKEN")
+    API_ID = os.environ.get("API_ID","8934899")
+    API_HASH = os.environ.get("API_HASH","bf3e98d2c351e4ad06946b4897374a1e")
+    TOKEN = os.environ.get("TOKEN","6604522716:AAEhGrGbdwkRm7Sl03J6W9p4GWd8xJ67fAs")
     START_PIC = os.environ.get("START_PIC")
-    CHAT = os.environ.get("CHAT")
+    CHAT = os.environ.get("CHAT","-1002136390368")
 
 
 
